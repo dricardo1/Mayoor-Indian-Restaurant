0832ce7fca66f94b45ab31104681244b50219100
Don Ricardo JR <dricardo1@gmail.com>
2020-11-12 15:31:06 -0500

    Menu section changes

--- js/text.js
+++ js/text.js
@@ -1,3 +1,4 @@
+/* Change message text */
 var messageArray = ["Explore Authentic Indian Cuisine.","Flavors as diverse as India itself!","Savor a pallette of fine tastes..."];
 var messageIndex = 0;
 
@@ -5,7 +6,7 @@ function changeMessage() {
 	// now grab the message and change it.
 	var newMessage = messageArray[messageIndex];
 	var messageElement = document.getElementById("mainMessage");
-	messageElement.innerHTML = newMessage;
+	messageElement.textContent = newMessage;
 	messageIndex++;
 	if (messageIndex >= messageArray.length) {
 		messageIndex = 0;
