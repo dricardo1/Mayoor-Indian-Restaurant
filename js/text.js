6e25d3978540e3f0b24c06b80227e20b2343875d
Don Ricardo JR <dricardo1@gmail.com>
2020-11-12 18:00:29 -0500

    mainMessage fix

--- js/text.js
+++ js/text.js
@@ -1,4 +1,4 @@
-var messageArray = ["Closed Due to Circumstance", "Explore Authentic Indian Cuisine","Flavors as diverse as India!","Closed Due to Circumstance","Savor a pallette of fine tastes..."];
+var messageArray = ["Closed Due to Circumstance", "Authentic Indian Cuisine","Flavors as diverse as India!","Closed Due to Circumstance","Savor a pallette of fine tastes..."];
 var messageIndex = 0;
 
 function changeMessage() {
